f9d71b3dfbf3e10decd907f2c1b6019b0c584c13
jvdirectorgeneral-svg <jvdirectorgeneral@mayuecuador.com>
2026-03-27 17:50:17 -0500

    Update models.py

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
+    password = Column(String, nullable=False)  # 👈 NUEVO
     status = Column(String, default="registered")
     membership_level = Column(Integer, nullable=True)
     membership_active = Column(Boolean, default=False)
