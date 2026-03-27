e365d6d28f8445b3351d9a7ee91c331375d50fac
jvdirectorgeneral-svg <jvdirectorgeneral@mayuecuador.com>
2026-03-27 11:28:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
+from database import engine, Base
 from users import router as users_router
+from models import User
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
