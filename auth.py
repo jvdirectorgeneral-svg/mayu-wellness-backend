1533553506d25cb90955f07a614ceb5c4b0291f8
jvdirectorgeneral-svg <jvdirectorgeneral@mayuecuador.com>
2026-03-27 18:23:51 -0500

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta, UTC
+from jose import jwt
+from passlib.context import CryptContext
+
+SECRET_KEY = "mayuclub_secret_key_2026"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
