776ed1b1ff6d6054f808c5b4326b21960de51f08
jvdirectorgeneral-svg <jvdirectorgeneral@mayuecuador.com>
2026-03-27 19:30:21 -0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -6,7 +6,7 @@ SECRET_KEY = "mayuclub_secret_key_2026"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
