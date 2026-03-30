885d3548158770a4ea82fb188360391049731c40
jvdirectorgeneral-svg <jvdirectorgeneral@mayuecuador.com>
2026-03-30 12:17:37 -0500

    Update products.py

--- products.py
+++ products.py
@@ -3,24 +3,16 @@ from sqlalchemy.orm import Session
 from database import get_db
 import models
 
-# 🔥 CREAR ROUTER
 router = APIRouter(prefix="/products", tags=["Products"])
 
 
-# =========================
-# OBTENER PRODUCTOS
-# =========================
 @router.get("/")
 def get_products(db: Session = Depends(get_db)):
     return db.query(models.Product).all()
 
 
-# =========================
-# SEED PRODUCTOS
-# =========================
 @router.post("/seed")
 def seed_products(db: Session = Depends(get_db)):
-
     products = [
         {"name": "Plata Coloidal", "price": 2, "description": "Coloide base"},
         {"name": "Cobre Coloidal", "price": 2, "description": "Coloide base"},
