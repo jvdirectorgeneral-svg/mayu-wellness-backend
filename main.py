7a2d36e2461ef4deb073ce4da8fba89647855488
jvdirectorgeneral-svg <jvdirectorgeneral@mayuecuador.com>
2026-06-02 11:21:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from paypal_subscriptions import router as paypal_subscriptions_router
 from superadmin import router as superadmin_router
 from marketing import router as marketing_router
 from marketing_uploads import router as marketing_uploads_router
+from marketplace import router as marketplace_router
 
 from dependencies import get_current_user
 import models
@@ -80,6 +81,7 @@ app.include_router(paypal_subscriptions_router)
 app.include_router(superadmin_router)
 app.include_router(marketing_router)
 app.include_router(marketing_uploads_router)
+app.include_router(marketplace_router)
 
 
 # =========================
